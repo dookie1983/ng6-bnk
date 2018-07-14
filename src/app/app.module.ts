c6467bff786219431ce006235a4780fa02d276bc
KoDookie <koh@odd.works>
2018-07-14 12:13:33 +0700

    add component and model bnk-gril-list bnk-girl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BnkGirlListComponent } from './components/bnk-girl-list/bnk-girl-list.component';
+import { BnkGirlComponent } from './components/bnk-girl/bnk-girl.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    BnkGirlListComponent,
+    BnkGirlComponent
   ],
   imports: [
     BrowserModule
